9f6e4128974bb9d9956becd31fc2e29d0214827f
JoshuaMarkle <joshuamarkle25@gmail.com>
2023-11-21 10:39:57 -0500

    added neotree

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -6,6 +6,9 @@ vim.api.nvim_set_keymap('n', 'tt', "<cmd>lua require('nvterm.terminal').toggle('
 vim.api.nvim_set_keymap('n', 'th', "<cmd>lua require('nvterm.terminal').toggle('horizontal')<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'tf', "<cmd>lua require('nvterm.terminal').toggle('float')<CR>", { noremap = true, silent = true })
 
+-- Map neo-tree to <leader-n>
+vim.api.nvim_set_keymap('n', '<Leader>n', ':Neotree<CR>', {noremap = true, silent = true})
+
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
