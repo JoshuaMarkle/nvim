dae7e8a0830353cc3b30ec3de37864fadb1e8dbb
JoshuaMarkle <joshuamarkle25@gmail.com>
2023-12-17 18:40:22 -0500

    remove dashboard; feels useless

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,6 +2,7 @@
 
 return {
 	'goolord/alpha-nvim',
+	enabled = false;
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	config = function ()
 		require'alpha'.setup(require'alpha.themes.theta'.config)
