92cb8a67d6aa4f4fd01750ee73d7feaa989605bf
JoshuaMarkle <joshuamarkle25@gmail.com>
2023-12-25 01:11:36 -0500

    actually decided to add full lsp support

--- lua/plugins/lsp/fidget.lua
+++ lua/plugins/lsp/fidget.lua
@@ -1,39 +1,39 @@
 return {
-  "j-hui/fidget.nvim",
-  tag = "legacy",
-  event = "LspAttach",
-  config = function()
-    local fidget_status_ok, fidget = pcall(require, "fidget")
-    if not fidget_status_ok then
-      print("fidget not found!")
-    end
-    fidget.setup({
-      text = {
-        spinner = {
-          "⣷",
-          "⣯",
-          "⣟",
-          "⡿",
-          "⢿",
-          "⣻",
-          "⣽",
-          "⣾",
-        },
-      },
-      timer = {
-        spinner_rate = 100,
-        fidget_decay = 3000,
-        task_decay = 3000,
-      },
-      window = {
-        relative = "editor",
-        blend = 0,
-        border = "rounded",
-      },
-      debug = {
-        logging = true,
-        strict = true,
-      },
-    })
-  end,
+	'j-hui/fidget.nvim',
+	tag = 'legacy',
+	event = 'LspAttach',
+	config = function()
+		local fidget_status_ok, fidget = pcall(require, 'fidget')
+		if not fidget_status_ok then
+			print 'fidget not found!'
+		end
+		fidget.setup {
+			text = {
+				spinner = {
+					'⣷',
+					'⣯',
+					'⣟',
+					'⡿',
+					'⢿',
+					'⣻',
+					'⣽',
+					'⣾',
+				},
+			},
+			timer = {
+				spinner_rate = 100,
+				fidget_decay = 3000,
+				task_decay = 3000,
+			},
+			window = {
+				relative = 'editor',
+				blend = 0,
+				border = 'rounded',
+			},
+			debug = {
+				logging = true,
+				strict = true,
+			},
+		}
+	end,
 }
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,15 +1,68 @@
--- Mason
-
 return {
-    "williamboman/mason.nvim",
-	event = 'BufReadPre',
+	'williamboman/mason.nvim',
+	event = { 'BufReadPost', 'BufNewFile' },
 	lazy = true,
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-      },
-    },
+	dependencies = {
+		{ 'williamboman/mason-lspconfig.nvim', cmd = { 'LspInstall', 'LspUninstall' } },
+	},
+	opts = {
+		ensure_installed = {
+			'stylua',
+			'spellcheck',
+			'shfmt',
+			'black',
+			'isort',
+			'flake8',
+			'ruff',
+			'eslint_d',
+			'luacheck',
+			'prettierd',
+		},
+	},
+	config = function()
+		local mason_status_ok, mason = pcall(require, 'mason')
+		if not mason_status_ok then
+			print 'mason not found!'
+		end
+		local mason_lspconfig_status_ok, mason_lspconfig = pcall(require, 'mason-lspconfig')
+		if not mason_lspconfig_status_ok then
+			print 'mason-lspconfig not found!'
+		end
+		local mason_null_ls_status_ok, mason_null_ls = pcall(require, 'mason-null-ls')
+		if not mason_null_ls_status_ok then
+			print 'mason-null-ls not found!'
+		end
+
+		mason.setup {
+			ui = {
+				icons = {
+					package_installed = ' ',
+					package_uninstalled = '○ ',
+					package_pending = ' ',
+				},
+			},
+		}
+		mason_lspconfig.setup {
+			ensure_installed = {
+				'tsserver',
+				'html',
+				'cssls',
+				'lua_ls',
+				'pyright',
+				'marksman',
+			},
+			automatic_installation = true,
+		}
+		mason_null_ls.setup {
+			ensure_installed = {
+				'prettierd',
+				'stylua',
+				'black',
+				'isort',
+				'ruff',
+				'eslint_d',
+				'luacheck',
+			},
+		}
+	end,
 }
